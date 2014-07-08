21af88ba8eeb54fa286d5153b937ed3f5ab3317d
Ivan Danci <bah4uk@gmail.com>
2014-07-08 11:33:40 +0300

    Remove trailing spaces

--- spec/ofx/ofx211_spec.rb
+++ spec/ofx/ofx211_spec.rb
@@ -5,19 +5,19 @@ describe OFX::Parser::OFX211 do
     @ofx = OFX::Parser::Base.new("spec/fixtures/v211.ofx")
     @parser = @ofx.parser
   end
-  
+
   it "should have a version" do
     OFX::Parser::OFX211::VERSION.should == "2.1.1"
   end
-  
+
   it "should set headers" do
     @parser.headers.should == @ofx.headers
   end
-  
+
   it "should set body" do
     @parser.body.should == @ofx.body
   end
-  
+
   it "should set account" do
     @parser.account.should be_a_kind_of(OFX::Account)
   end
@@ -51,7 +51,7 @@ describe OFX::Parser::OFX211 do
       before do
         @t = @transactions[1]
       end
-      
+
       it "should contain the correct values" do
         @t.amount.should == -23
         @t.fit_id.should == "219867"
@@ -60,12 +60,12 @@ describe OFX::Parser::OFX211 do
         @t.name.should == "Interest Charge"
       end
     end
-    
+
     context "third" do
       before do
         @t = @transactions[2]
       end
-      
+
       it "should contain the correct values" do
         @t.amount.should == 350
         @t.fit_id.should == "219868"
