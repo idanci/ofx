4ca5d82cd62b13df9508c15b30f1a8fa3ecc2863
Ivan Danci <bah4uk@gmail.com>
2014-07-10 18:08:29 +0300

    Add specs for accounts list

--- spec/ofx/account_spec.rb
+++ spec/ofx/account_spec.rb
@@ -10,40 +10,40 @@ describe OFX::Account do
     context "Bank account" do
       it "should return currency" do
         account.currency.should == "BRL"
-        hash[:currency].should == "BRL"
+        hash[:currency].should == account.currency
       end
 
       it "should return bank id" do
         account.bank_id.should == "0356"
-        hash[:bank_id].should == "0356"
+        hash[:bank_id].should == account.bank_id
       end
 
       it "should return id" do
         account.id.should == "03227113109"
-        hash[:id].should == "03227113109"
+        hash[:id].should == account.id
       end
 
       it "should return type" do
         account.type.should == :checking
-        hash[:type].should == :checking
+        hash[:type].should == account.type
       end
 
       it "should return transactions" do
         account.transactions.should be_a_kind_of(Array)
         account.transactions.size.should == 36
         hash[:transactions].should be_a_kind_of(Array)
-        hash[:transactions].size.should == 36
+        hash[:transactions].size.should == account.transactions.size
         hash[:transactions].first.should == account.transactions.first.to_hash
       end
 
       it "should return balance" do
         account.balance.amount.should == 598.44
-        hash[:balance][:amount].should == 598.44
+        hash[:balance][:amount].should == account.balance.amount
       end
 
       it "should return balance in pennies" do
         account.balance.amount_in_pennies.should == 59844
-        hash[:balance][:amount_in_pennies].should == 59844
+        hash[:balance][:amount_in_pennies].should == account.balance.amount_in_pennies
       end
 
       it "should return balance date" do
@@ -54,17 +54,17 @@ describe OFX::Account do
       context "available_balance" do
         it "should return available balance" do
           account.available_balance.amount.should == 1555.99
-          hash[:available_balance][:amount].should == 1555.99
+          hash[:available_balance][:amount].should == account.available_balance.amount
         end
 
         it "should return available balance in pennies" do
           account.available_balance.amount_in_pennies.should == 155599
-          hash[:available_balance][:amount_in_pennies].should == 155599
+          hash[:available_balance][:amount_in_pennies].should == account.available_balance.amount_in_pennies
         end
 
         it "should return available balance date" do
           account.available_balance.posted_at.should == Time.parse("2009-11-01")
-          hash[:available_balance][:posted_at].should == Time.parse("2009-11-01")
+          hash[:available_balance][:posted_at].should == account.available_balance.posted_at
         end
 
         it "should return nil if AVAILBAL not found" do
